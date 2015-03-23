b4f349a73ce9dfd63337933d461f85cbb7b63ff1
Patrick Kozma <patrick@12finger.com>
2015-03-23 11:54:26 +0100

    get rid of locale detector
    as we have our own

--- lib/route_translator/extensions.rb
+++ lib/route_translator/extensions.rb
@@ -1,3 +1,2 @@
 require File.expand_path('../extensions/mapper', __FILE__)
 require File.expand_path('../extensions/route_set', __FILE__)
-require File.expand_path('../extensions/action_controller', __FILE__)
